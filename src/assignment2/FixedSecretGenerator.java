6a4752ce0206e7e433fc30421f35d3105f009e11
jason <jasonkuchler@gmail.com>
2026-09-15 17:08:46 -0500

    Implements FixedSecretGenerator.

--- src/assignment2/FixedSecretGenerator.java
+++ src/assignment2/FixedSecretGenerator.java
@@ -1,5 +1,12 @@
 package assignment2;
 
 public class FixedSecretGenerator implements SecretGenerator{
-
+    private Code code;
+    public FixedSecretGenerator(Code code){
+        this.code = code;
+    }
+    @Override
+    public Code generate(GameConfiguration config) {
+        return code;
+    }
 }
