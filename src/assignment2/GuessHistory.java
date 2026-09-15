fa95d4941ab7ee516cebb916512be39737ebe1f0
jason <jasonkuchler@gmail.com>
2026-09-15 18:11:01 -0500

    Added toString to GuessHistory. Need to add driver.java

--- src/assignment2/GuessHistory.java
+++ src/assignment2/GuessHistory.java
@@ -14,4 +14,17 @@ public class GuessHistory {
     public List<GuessRecord> getRecords(){
         return new ArrayList<>(records);
     }
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("Guess --> Result").append(System.lineSeparator());
+        for (int i = 0; i < records.size(); i++) {
+            GuessRecord record = records.get(i);
+            result.append(record.getGuess());
+            result.append(" --> ");
+            result.append(record.getResult());
+            result.append(System.lineSeparator());
+        }
+        return result.toString();
+    }
 }
